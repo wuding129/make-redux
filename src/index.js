09b1f9a7aa864475dcacfd1e77f2a1496f782473
wuding <wuding129@163.com>
2017-05-03 08:02:18 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-const appState = {
-  title: {
-    text: 'React.js',
-    color: 'blue'
-  },
-  content: {
-    text: 'angular4',
-    color: 'red'
-  }
-}
+// const appState = {
+//   title: {
+//     text: 'React.js',
+//     color: 'blue'
+//   },
+//   content: {
+//     text: 'angular4',
+//     color: 'red'
+//   }
+// }
 
 
 function renderApp(newAppState, oldAppState = {}) {
   if (newAppState === oldAppState) return
-  renderTitle(newAppState.title)
-  renderContent(newAppState.content)
+  console.log('render app...')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
 }
 
 function renderTitle(newTitle, oldTitle = {}) {
   if (newTitle === oldTitle) return
+  console.log('render title...')
   const titleDOM = document.getElementById('title')
   titleDOM.innerHTML = newTitle.text
   titleDOM.style.color = newTitle.color
@@ -25,6 +27,7 @@ function renderTitle(newTitle, oldTitle = {}) {
 
 function renderContent(newContent, oldContent = {}) {
   if (newContent === oldContent) return
+  console.log('render content')
   const contentDOM = document.getElementById('content')
   contentDOM.innerHTML = newContent.text
   contentDOM.style.color = newContent.color
@@ -52,31 +55,73 @@ function renderContent(newContent, oldContent = {}) {
 // renderApp(appState)
 
 
-function createStore(state, stateChanger) {
+function createStore(reducer) {
+  let state = null
   const listeners = []
   const subscribe = (listener) => listeners.push(listener)
   const getState = () => state
   const dispatch = (action) => {
-    stateChanger(state, action)
+    state = reducer(state, action)
     listeners.forEach((listener) => listener())
   }
+  dispatch({})
   return { getState, dispatch, subscribe}
 }
 
-function stateChanger(state, action) {
+function reducer(state, action) {
+  if (!state){
+    return {
+      title: {
+        text: 'react.js',
+        color: 'red'
+      },
+      content: {
+        text: 'angular',
+        color: 'blue'
+      }
+    }
+  }
   switch (action.type) {
     case 'UPDATE_TITLE_TEXT':
-      state.title.text = action.text
-      break
+      // 构建新的对象并且返回
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
     case 'UPDATE_TITLE_COLOR':
-      state.title.color = action.color
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    case 'UPDATE_CONTENT_TEXT':
+      // 构建新的对象并且返回
+      return {
+        ...state,
+        content: {
+          ...state.content,
+          text: action.text
+        }
+      }
+    case 'UPDATE_CONTENT_COLOR':
+      return {
+        ...state,
+        content: {
+          ...state.content,
+          color: action.color
+        }
+      }
     default:
-      break
+      return state
   }
 }
 
-const store = createStore(appState, stateChanger)
+const store = createStore(reducer)
 let oldStore = store.getState()
 store.subscribe(()=> {
   const newState = store.getState()
@@ -85,6 +130,7 @@ store.subscribe(()=> {
 })
 
 renderApp(store.getState()) // 首次渲染
-store.dispatch({type: 'UPDATE_TITLE_TEXT', text: 'React+Redux'})
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: 'React'})
 store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'green'})
+store.dispatch({type: 'UPDATE_CONTENT_COLOR', color: 'green'})
 // renderApp(store.getState())
